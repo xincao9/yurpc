2e1621e9aebfbb984f0fb7d2db774d5c9184a11b
xin.cao <xin.cao@asiainnovations.com>
2019-03-26 19:25:02 +0800

    ok

--- jsonrpc-core/src/main/java/com/github/xincao9/jsonrpc/core/util/HostUtils.java
+++ jsonrpc-core/src/main/java/com/github/xincao9/jsonrpc/core/util/HostUtils.java
@@ -16,23 +16,51 @@
 package com.github.xincao9.jsonrpc.core.util;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * 主机工具
+ * 
  * @author xincao9@gmail.com
  */
 public class HostUtils {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HostUtils.class);
 
-    public static String getLocalAddress () {
+    /**
+     * 获得本机IP地址
+     * 
+     * @return 本机IP
+     */
+    public static String getLocalAddress() {
         try {
-            InetAddress addr = InetAddress.getLocalHost();
-            return addr.getHostAddress();
-        } catch (UnknownHostException e) {
+            InetAddress candidateAddress = null; // 遍历所有的网络接口
+            Enumeration ifaces = NetworkInterface.getNetworkInterfaces();
+            while (ifaces.hasMoreElements()) {
+                NetworkInterface iface = (NetworkInterface) ifaces.nextElement(); // 在所有的接口下再遍历IP
+                Enumeration inetAddrs = iface.getInetAddresses();
+                while (inetAddrs.hasMoreElements()) {
+                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+                    if (!inetAddr.isLoopbackAddress()) { // 排除loopback类型地址
+                        if (inetAddr.isSiteLocalAddress()) { // 如果是site-local地址，就是它了
+                            return inetAddr.getHostAddress();
+                        } else if (candidateAddress == null) {  // site-local类型的地址未被发现，先记录候选地址
+                            candidateAddress = inetAddr;
+                        }
+                    }
+                }
+            }
+            if (candidateAddress != null) {
+                return candidateAddress.getHostAddress();
+            }
+            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost(); // 如果没有发现 non-loopback地址.只能用最次选的方案
+            return jdkSuppliedAddress.getHostAddress();
+        } catch (SocketException | UnknownHostException e) {
             LOGGER.error(e.getMessage());
         }
         return null;
