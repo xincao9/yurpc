9af0de2b74852971838f98f28ff2a80b5d794f5a
xin.cao <xin.cao@asiainnovations.com>
2019-04-08 15:46:23 +0800

    ok

--- jsonrpc-ui/src/main/java/com/github/xincao9/jsonrpc/ui/repository/TimerDAO.java
+++ jsonrpc-ui/src/main/java/com/github/xincao9/jsonrpc/ui/repository/TimerDAO.java
@@ -2,6 +2,7 @@ package com.github.xincao9.jsonrpc.ui.repository;
 
 import com.github.xincao9.jsonrpc.core.constant.MetricConsts;
 import com.github.xincao9.jsonrpc.ui.entity.Timer;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +19,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class TimerDAO {
 
-    private static final String TIMER_BY_METHOD = "select method, ct, sum(one_minute_rate) as one_minute_rate, sum(five_minute_rate) as five_minute_rate, sum(fifteen_minute_rate) as fifteen_minute_rate from timer where method=:method group by ct order by ct limit 100";
+    private static final String TIMER_BY_METHOD = "select method, ct, sum(one_minute_rate) as one_minute_rate, sum(five_minute_rate) as five_minute_rate, sum(fifteen_minute_rate) as fifteen_minute_rate from timer where method=:method group by ct order by ct desc limit 100";
     private static final String METHODS = "select method from timer group by method";
     private static final String SAVE = "insert into timer (host, port, method, count, one_minute_rate, five_minute_rate, fifteen_minute_rate, ct) values (:host,:port,:method,:count,:one_minute_rate,:five_minute_rate,:fifteen_minute_rate,:ct)";
 
@@ -29,7 +30,7 @@ public class TimerDAO {
         Map<String, Object> map = new HashMap();
         map.put("method", method);
         SqlParameterSource sqlParameterSource = new MapSqlParameterSource(map);
-        return namedParameterJdbcDaoSupport.getNamedParameterJdbcTemplate().query(TIMER_BY_METHOD, sqlParameterSource, (rs, rowNum) -> {
+        List<Timer> timers =  namedParameterJdbcDaoSupport.getNamedParameterJdbcTemplate().query(TIMER_BY_METHOD, sqlParameterSource, (rs, rowNum) -> {
             Timer timer = new Timer();
             timer.setMethod(rs.getString(MetricConsts.METHOD));
             timer.setCt(rs.getString("ct"));
@@ -38,6 +39,8 @@ public class TimerDAO {
             timer.setFifteenMinuteRate(rs.getLong(MetricConsts.FIFTEEN_MINUTE_RATE));
             return timer;
         });
+        Collections.reverse(timers);
+        return timers;
     }
 
     public List<String> getMethods () {
